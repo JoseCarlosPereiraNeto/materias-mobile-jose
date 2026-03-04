ff49953b795de7b6d4ba721223f192cb3fbf1e0e
JoseCarlosPereiraNeto <jose.c.neto@sou.unifeob.edu.br>
2026-03-04 15:30:46 -0300

    text botao

--- Flutter/app_fluxolivre/lib/src/pages/home_page.dart
+++ Flutter/app_fluxolivre/lib/src/pages/home_page.dart
@@ -12,32 +12,63 @@ class HomePage extends StatelessWidget {
           image: DecorationImage(
             image: AssetImage('assets/images/img_fundo.png'),
             fit: BoxFit.cover,
-            )
+          ),
         ),
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            Text("Acesso Liberado",
-                style: TextStyle(
-                  color: Color(0xFFFFFFFF),
-                  fontSize: 40,
-                  fontWeight: FontWeight.w500,
-            ),              
+            Text(
+              "Acesso Liberado",
+              style: TextStyle(
+                color: Color(0xFFFFFFFF),
+                fontSize: 40,
+                fontWeight: FontWeight.w500,
+              ),
             ),
-            Text("0",
-                style: TextStyle(
-                  color: Color(0xFFFFFFFF),
-                  fontSize: 60,
-                  fontWeight: FontWeight.w500,
-                )
+            Text(
+              "0",
+              style: TextStyle(
+                color: Color(0xFFFFFFFF),
+                fontSize: 60,
+                fontWeight: FontWeight.w500,
+              ),
             ),
-            Row(mainAxisAlignment: MainAxisAlignment.spaceEvenly, children: [
-              ElevatedButton(onPressed: void Hey() => ola, child: Text("ola", style: TextStyle(color: Color(0xFFFFFFFF), fontSize: 60, fontWeight: FontWeight.w500))),
-              ElevatedButton(onPressed: null, child: Text("mundo", style: TextStyle(color: Color(0xFFFFFFFF), fontSize: 60, fontWeight: FontWeight.w500)))
-            ])
-            ],
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                Container(
+                  decoration: BoxDecoration(
+                    color: Color.fromARGB(255, 34, 139, 34),
+                  ),
+                  child: ElevatedButton(
+                    onPressed: null,
+                    child: Text(
+                      "Sair",
+                      style: TextStyle(
+                        color: Color.fromARGB(255, 150, 139, 3),
+                        fontSize: 60,
+                        fontWeight: FontWeight.w500,
+                      ),
+                    ),
+                  ),
+                ),
+
+                ElevatedButton(
+                  onPressed: null,
+                  child: Text(
+                    "Entar",
+                    style: TextStyle(
+                      color: Color.fromARGB(255, 75, 31, 146),
+                      fontSize: 60,
+                      fontWeight: FontWeight.w500,
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ],
         ),
-      )
+      ),
     );
   }
 }
